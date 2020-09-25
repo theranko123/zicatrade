fd00d04590d97b99593896360a80731411540227
RadeStojicic <54794837+RadeStojicic@users.noreply.github.com>
2020-09-25 13:42:39 +0200

    Navbar

--- zicatrade-master/index.js
+++ zicatrade-master/index.js
@@ -1,1 +1,6 @@
+// let proizvodi = document.querySelector(".proizvodi")
+// let proizvodi_links = document.querySelector(".proizvodi_links")
 
+// proizvodi.addEventListener("click", e => {
+//     proizvodi_links.style.display = "block"
+// })
