4ec26c983a09aa13da1e0c1a39d347d67e1ed1da
theranko123 <71515678+theranko123@users.noreply.github.com>
2020-09-27 16:31:47 +0200

    Delete index.js

--- zicatrade-master/index.js
+++
@@ -1,6 +0,0 @@
-// let proizvodi = document.querySelector(".proizvodi")
-// let proizvodi_links = document.querySelector(".proizvodi_links")
-
-// proizvodi.addEventListener("click", e => {
-//     proizvodi_links.style.display = "block"
-// })
